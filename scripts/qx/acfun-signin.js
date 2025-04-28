3e3966f61078a9a0ebc0261cd29c4fb70ee7b3f9
happy0853077 <happy08530777@gmail.com>
2025-04-28 10:47:41 +0800

    Update acfun-signin.js

--- scripts/qx/acfun-signin.js
+++ scripts/qx/acfun-signin.js
@@ -1,37 +1,15 @@
 const ScriptName = 'AcFun-signin';
 
-const cookieKey = 'cookie_acfun';
-const tokenKey = 'token_acfun';
-const cookieVal = $prefs.valueForKey(cookieKey);
-const tokenVal = $prefs.valueForKey(tokenKey);
+const times = 3;
+const interval = 1000;
+const timeout = 7000;
 
-console.log(tokenVal);
-console.log(cookieVal);
-
-const headers = {
-    Cookie: cookieVal,
-    access_token: `${tokenVal}`,
-    acPlatform: 'IPHONE',
-    'User-Agent': 'AcFun/6.14.2 (iPhone; iOS 13.3; Scale/2.00)'
-};
 
 if ($environment.executor == 'cron' || $environment.executor == 'test-purpose') {
-    sign();
-} else if ($environment.executor == 'rewrite-request-body') {
-    if ('access_token' in $request.headers) {
-        console.log('get token: ' + $request.headers['access_token']);
-        if ($prefs.ValueForKey(tokenKey) != $request.headers['access_token']) {
-            $notify(ScriptName, '', 'token获取成功');
-            $prefs.setValueForKey($request.headers['access_token'], tokenKey);
-        }
-    }
-    if ('Cookie' in $request.headers) {
-        console.log('get cookie: ' + $request.headers['Cookie']);
-        if ($prefs.ValueForKey(cookieKey) != $request.headers['cookie']) {
-            $notify(ScriptName, '', 'cookie获取成功');
-            $prefs.setValueForKey($request.headers['Cookie'], cookieKey);
-        }
-    }
+    main();
+
+} else if ($environment.executor == 'rewrite-request-header') {
+    getToken();
     $done();
 } else {
     console.log(`unknown executor: ${$environment.executor}`);
@@ -39,70 +17,102 @@ if ($environment.executor == 'cron' || $environment.executor == 'test-purpose') 
 }
 
 
-function sign() {
-    const request = {
-        url: `https://api-new.acfunchina.com/rest/app/user/signIn`,
-        headers: headers,
-        body: '',
-        method: 'POST'
-    };
-    $task.fetch(request).then(response => {
-        const result = JSON.parse(response.body);
-        if (result.result == 0 || result.result == 122) {
-            request.url = `https://api-new.acfunchina.com/rest/app/user/hasSignedIn`;
-            $task.fetch(request).then(response => {
-                const checkresult = JSON.parse(response.body);
-                let subTitle;
-                let detail;
-                if (result.result == 0) {
-                    subTitle = `✅签到成功`;
-                    const regex = /(\d+蕉)/;
-                    let bananaCount = result.msg.match(regex)[1];
-                    detail = `连续签到${checkresult.continuousDays}天，获得${bananaCount}`;
-                } else if (result.result == 122) {
-                    subTitle = `❇️重复签到`;
-                    detail = `连续签到${checkresult.continuousDays}天，${result.msg}`;
-                } else {
-                    console.log('unknown code: ' + result.result);
-                    subTitle = '⚠️签到异常';
-                    detail = `错误：${result.result}`;
-                }
-                $notify(ScriptName, subTitle, detail);
-                $done();
-            });
-        } else {
-            $notify(ScriptName, `❌签到失败`, `编码: ${result.result}, 说明: ${result.error_msg}`);
+async function main() {
+
+    for (let i = 0; i < times; i ++) {
+        if (await signTimeout(timeout) == 0) {
             $done();
+        } else {
+            console.log((i + 1) + ' failed');
         }
-    });
+        await new Promise(resolve => setTimeout(resolve, interval));
+    }
+    $notify(ScriptName, '❌签到失败', '运行超时，请重试');
+    $done();
+}
+
+async function signTimeout(time) {
+
+    return Promise.race([
+        sign(),
+        new Promise((_, reject) => 
+            setTimeout(() => 1, time)
+        )
+    ]);
 }
 
 
-function getinfo(signresult) {
-    let request = {
-        url: `https://api-new.acfunchina.com/rest/app/user/hasSignedIn`,
-        headers: headers,
+async function sign() {
+
+    const request = {
+        url: `https://api-new.acfunchina.com/rest/app/user/signIn`,
+        headers: {
+            Cookie: $prefs.valueForKey('cookie_acfun'),
+            access_token: $prefs.valueForKey('token_acfun'),
+            acPlatform: 'IPHONE',
+            'User-Agent': 'AcFun/6.14.2 (iPhone; iOS 13.3; Scale/2.00)'
+        },
         body: '',
         method: 'POST'
     };
-    $task.fetch(request).then(response => {
-        const result = JSON.parse(response.body);
+
+    const response = await $task.fetch(request);
+    const result = JSON.parse(response.body);
+    console.log(response.body);
+
+    if (result.result == 0 || result.result == 122) {
+
+        request.url = `https://api-new.acfunchina.com/rest/app/user/hasSignedIn`;
+
+        const checkResponse = await $task.fetch(request);
+        console.log(checkResponse.body);
+        const checkresult = JSON.parse(checkResponse.body);
+
         let subTitle;
         let detail;
-        if (signresult.result == 0) {
+
+        if (result.result == 0) {
             subTitle = `✅签到成功`;
             const regex = /(\d+蕉)/;
-            let bananaCount = signresult.msg.match(regex)[1];
-            detail = `连续签到${result.continuousDays}天，获得${bananaCount}`;
-        } else if (signresult.result == 122) {
+            let bananaCount = result.msg.match(regex)[1];
+            detail = `连续签到${checkresult.cumulativeDays}天，获得${bananaCount}`;
+
+        } else if (result.result == 122) {
             subTitle = `❇️重复签到`;
-            detail = `连续签到${result.continuousDays}天，${signresult.msg}`;
+            detail = `连续签到${checkresult.cumulativeDays}天，${result.msg}`;
+
         } else {
-            console.log('unknown code: ' + signresult.result);
+            console.log('unknown code: ' + result.result);
             subTitle = '⚠️签到异常';
-            detail = `错误：${signresult.result}`;
+            detail = `错误：${result.result}`;
         }
+
         $notify(ScriptName, subTitle, detail);
-        $done();
-    });
+        return 0;
+
+    } else {
+        $notify(ScriptName, `❌签到失败`, `编码: ${result.result}, 说明: ${result.error_msg}`);
+        return 1;
+    }
+}
+
+
+function getToken() {
+
+    if ('access_token' in $request.headers) {
+
+        console.log('get token: ' + $request.headers['access_token']);
+        if ($prefs.ValueForKey('token_acfun') != $request.headers['access_token']) {
+            $notify(ScriptName, '', 'token获取成功');
+            $prefs.setValueForKey($request.headers['access_token'], 'token_acfun');
+        }
+    }
+    if ('Cookie' in $request.headers) {
+
+        console.log('get cookie: ' + $request.headers['Cookie']);
+        if ($prefs.ValueForKey('cookie_acfun') != $request.headers['cookie']) {
+            $notify(ScriptName, '', 'cookie获取成功');
+            $prefs.setValueForKey($request.headers['Cookie'], 'cookie_acfun');
+        }
+    }
 }
